e55ffe7bcb730b001d792b7a77d3329ec8c855f2
Brian Brown <brian.brown473@gmail.com>
2014-02-03 23:33:07 -0500

    commit 2

--- convolutional_mlp.py
+++ convolutional_mlp.py
@@ -27,6 +27,8 @@ import os
 import sys
 import time
 
+import pa3util
+
 import numpy
 
 import theano
@@ -103,7 +105,7 @@ class LeNetConvPoolLayer(object):
         self.params = [self.W, self.b]
 
 
-def evaluate_lenet5(learning_rate=0.1, n_epochs=2,
+def evaluate_lenet5(learning_rate=0.1, n_epochs=5,
                     dataset='mnist.pkl.gz',
                     nkerns=[20, 50], batch_size=500):
     """ Demonstrates lenet on MNIST dataset
@@ -121,22 +123,34 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=2,
     :type nkerns: list of ints
     :param nkerns: number of kernels on each layer
     """
+    image_size = 32
 
     rng = numpy.random.RandomState(23455)
 
-    datasets = load_data(dataset)
+    print "... loading data"
+    
+    train_set = numpy.load('train_data.npy')
+    train_labels = numpy.load('train_labels.npy')
+    test_set_x = numpy.load('test_data.npy')
+    
+    #train_set_img = pa3util.data_to_img(train_set[0])
+    
+    #for i in range(1, 5000):
+	#	train_set_img = numpy.append(train_set_img, pa3util.data_to_img(train_set[i]), axis=0)
 
-    train_set_x, train_set_y = datasets[0]
-    valid_set_x, valid_set_y = datasets[1]
-    test_set_x, test_set_y = datasets[2]
+    train_set_x = T.cast(train_set[:2500], dtype='float64')
+    train_set_y = T.cast(train_labels[:2500], dtype='int32')
+	
+    valid_set_x = T.cast(train_set[2500:], dtype='float64')
+    valid_set_y = T.cast(train_labels[2500:], dtype='int32')
 
     # compute number of minibatches for training, validation and testing
-    n_train_batches = train_set_x.get_value(borrow=True).shape[0]
-    n_valid_batches = valid_set_x.get_value(borrow=True).shape[0]
-    n_test_batches = test_set_x.get_value(borrow=True).shape[0]
+    n_train_batches = train_set_x.shape[0]
+    n_valid_batches = valid_set_x.shape[0]
+    #n_test_batches = test_set_x.get_value(borrow=True).shape[0]
     n_train_batches /= batch_size
     n_valid_batches /= batch_size
-    n_test_batches /= batch_size
+    #n_test_batches /= batch_size
 
     # allocate symbolic variables for the data
     index = T.lscalar()  # index to a [mini]batch
@@ -144,7 +158,7 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=2,
     y = T.ivector('y')  # the labels are presented as 1D vector of
                         # [int] labels
 
-    ishape = (28, 28)  # this is the size of MNIST images
+    ishape = (image_size, image_size)  # this is the size of the images
 
     ######################
     # BUILD ACTUAL MODEL #
@@ -153,22 +167,22 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=2,
 
     # Reshape matrix of rasterized images of shape (batch_size,28*28)
     # to a 4D tensor, compatible with our LeNetConvPoolLayer
-    layer0_input = x.reshape((batch_size, 1, 28, 28))
+    layer0_input = x.reshape((2500, 3, image_size, image_size))
 
     # Construct the first convolutional pooling layer:
     # filtering reduces the image size to (28-5+1,28-5+1)=(24,24)
     # maxpooling reduces this further to (24/2,24/2) = (12,12)
     # 4D output tensor is thus of shape (batch_size,nkerns[0],12,12)
     layer0 = LeNetConvPoolLayer(rng, input=layer0_input,
-            image_shape=(batch_size, 1, 28, 28),
-            filter_shape=(nkerns[0], 1, 5, 5), poolsize=(2, 2))
+            image_shape=(2500, 3, image_size, image_size),
+            filter_shape=(nkerns[0], 3, 5, 5), poolsize=(2, 2))
 
     # Construct the second convolutional pooling layer
     # filtering reduces the image size to (12-5+1,12-5+1)=(8,8)
     # maxpooling reduces this further to (8/2,8/2) = (4,4)
     # 4D output tensor is thus of shape (nkerns[0],nkerns[1],4,4)
     layer1 = LeNetConvPoolLayer(rng, input=layer0.output,
-            image_shape=(batch_size, nkerns[0], 12, 12),
+            image_shape=(2500, nkerns[0], 14, 14),
             filter_shape=(nkerns[1], nkerns[0], 5, 5), poolsize=(2, 2))
 
     # the HiddenLayer being fully-connected, it operates on 2D matrices of
@@ -177,25 +191,25 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=2,
     layer2_input = layer1.output.flatten(2)
 
     # construct a fully-connected sigmoidal layer
-    layer2 = HiddenLayer(rng, input=layer2_input, n_in=nkerns[1] * 4 * 4,
-                         n_out=500, activation=T.tanh)
+    layer2 = HiddenLayer(rng, input=layer2_input, n_in=1250,
+                         n_out=2500, activation=T.tanh)
 
     # classify the values of the fully-connected sigmoidal layer
-    layer3 = LogisticRegression(input=layer2.output, n_in=500, n_out=10)
+    layer3 = LogisticRegression(input=layer2.output, n_in=2500, n_out=2)
 
     # the cost we minimize during training is the NLL of the model
     cost = layer3.negative_log_likelihood(y)
 
     # create a function to compute the mistakes that are made by the model
-    test_model = theano.function([index], layer3.errors(y),
-             givens={
-                x: test_set_x[index * batch_size: (index + 1) * batch_size],
-                y: test_set_y[index * batch_size: (index + 1) * batch_size]})
+    #test_model = theano.function([index], layer3.errors(y),
+    #         givens={
+    #            x: test_set_x[index * batch_size: (index + 1) * batch_size],
+    #            y: test_set_y[index * batch_size: (index + 1) * batch_size]})
 
     validate_model = theano.function([index], layer3.errors(y),
             givens={
-                x: valid_set_x[index * batch_size: (index + 1) * batch_size],
-                y: valid_set_y[index * batch_size: (index + 1) * batch_size]})
+                x: valid_set_x,
+                y: valid_set_y}, on_unused_input='ignore')
 
     # create a list of all model parameters to be fit by gradient descent
     params = layer3.params + layer2.params + layer1.params + layer0.params
@@ -214,20 +228,20 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=2,
 
     train_model = theano.function([index], cost, updates=updates,
           givens={
-            x: train_set_x[index * batch_size: (index + 1) * batch_size],
-            y: train_set_y[index * batch_size: (index + 1) * batch_size]})
+            x: train_set_x,
+            y: train_set_y}, on_unused_input='ignore')
 
     ###############
     # TRAIN MODEL #
     ###############
     print '... training'
     # early-stopping parameters
-    patience = 10000  # look as this many examples regardless
-    patience_increase = 2  # wait this much longer when a new best is
+    #patience = 10000  # look as this many examples regardless
+    #patience_increase = 2  # wait this much longer when a new best is
                            # found
-    improvement_threshold = 0.995  # a relative improvement of this much is
+    #improvement_threshold = 0.995  # a relative improvement of this much is
                                    # considered significant
-    validation_frequency = min(n_train_batches, patience / 2)
+    #validation_frequency = 1
                                   # go through this many
                                   # minibatche before checking the network
                                   # on the validation set; in this case we
@@ -242,58 +256,24 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=2,
     epoch = 0
     done_looping = False
 
-    while (epoch < n_epochs) and (not done_looping):
+    while epoch < n_epochs:
         epoch = epoch + 1
-        for minibatch_index in xrange(n_train_batches):
+        
+        cost_ij = train_model(1)
+           
+        validation_losses = validate_model(1)
+                
+        print validation_losses
 
-            iter = (epoch - 1) * n_train_batches + minibatch_index
-
-            if iter % 100 == 0:
-                print 'training @ iter = ', iter
-            cost_ij = train_model(minibatch_index)
-
-            if (iter + 1) % validation_frequency == 0:
-
-                # compute zero-one loss on validation set
-                validation_losses = [validate_model(i) for i
-                                     in xrange(n_valid_batches)]
-                this_validation_loss = numpy.mean(validation_losses)
-                print('epoch %i, minibatch %i/%i, validation error %f %%' % \
-                      (epoch, minibatch_index + 1, n_train_batches, \
-                       this_validation_loss * 100.))
-
-                # if we got the best validation score until now
-                if this_validation_loss < best_validation_loss:
-
-                    #improve patience if loss improvement is good enough
-                    if this_validation_loss < best_validation_loss *  \
-                       improvement_threshold:
-                        patience = max(patience, iter * patience_increase)
-
-                    # save best validation score and iteration number
-                    best_validation_loss = this_validation_loss
-                    best_iter = iter
-
-                    # test it on the test set
-                    test_losses = [test_model(i) for i in xrange(n_test_batches)]
-                    test_score = numpy.mean(test_losses)
-                    print(('     epoch %i, minibatch %i/%i, test error of best '
-                           'model %f %%') %
-                          (epoch, minibatch_index + 1, n_train_batches,
-                           test_score * 100.))
-
-            if patience <= iter:
-                done_looping = True
-                break
 
     end_time = time.clock()
-    print('Optimization complete.')
-    print('Best validation score of %f %% obtained at iteration %i,'\
-          'with test performance %f %%' %
-          (best_validation_loss * 100., best_iter + 1, test_score * 100.))
-    print >> sys.stderr, ('The code for file ' +
-                          os.path.split(__file__)[1] +
-                          ' ran for %.2fm' % ((end_time - start_time) / 60.))
+    #print('Optimization complete.')
+    #print('Best validation score of %f %% obtained at iteration %i,'\
+    #      'with test performance %f %%' %
+    #      (best_validation_loss * 100., best_iter + 1, test_score * 100.))
+    #print >> sys.stderr, ('The code for file ' +
+    #                      os.path.split(__file__)[1] +
+    #                      ' ran for %.2fm' % ((end_time - start_time) / 60.))
 
 if __name__ == '__main__':
     evaluate_lenet5()

6a2803ce094dce00e5dbfa1931629459c1e5c77b
Brian Brown <brian.brown473@gmail.com>
2014-02-06 21:46:26 -0500

    Fixed CNN

--- convolutional_mlp.py
+++ convolutional_mlp.py
@@ -105,9 +105,9 @@ class LeNetConvPoolLayer(object):
         self.params = [self.W, self.b]
 
 
-def evaluate_lenet5(learning_rate=0.1, n_epochs=5,
+def evaluate_lenet5(learning_rate=0.0005, n_epochs=50,
                     dataset='mnist.pkl.gz',
-                    nkerns=[20, 50], batch_size=500):
+                    nkerns=[20, 50], batch_size=10):
     """ Demonstrates lenet on MNIST dataset
 
     :type learning_rate: float
@@ -129,9 +129,9 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=5,
 
     print "... loading data"
     
-    train_set = numpy.load('train_data.npy')
-    train_labels = numpy.load('train_labels.npy')
-    test_set_x = numpy.load('test_data.npy')
+    train_set = theano.shared(numpy.load('train_data.npy'))
+    train_labels = theano.shared(numpy.load('train_labels.npy'))
+    test_set_x = theano.shared(numpy.load('test_data.npy'))
     
     #train_set_img = pa3util.data_to_img(train_set[0])
     
@@ -145,11 +145,11 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=5,
     valid_set_y = T.cast(train_labels[2500:], dtype='int32')
 
     # compute number of minibatches for training, validation and testing
-    n_train_batches = train_set_x.shape[0]
-    n_valid_batches = valid_set_x.shape[0]
+    #n_train_batches = train_set_x.shape[0]
+    #n_valid_batches = valid_set_x.shape[0]
     #n_test_batches = test_set_x.get_value(borrow=True).shape[0]
-    n_train_batches /= batch_size
-    n_valid_batches /= batch_size
+    #n_train_batches /= batch_size
+    #n_valid_batches /= batch_size
     #n_test_batches /= batch_size
 
     # allocate symbolic variables for the data
@@ -167,14 +167,14 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=5,
 
     # Reshape matrix of rasterized images of shape (batch_size,28*28)
     # to a 4D tensor, compatible with our LeNetConvPoolLayer
-    layer0_input = x.reshape((2500, 3, image_size, image_size))
+    layer0_input = x.reshape((batch_size, 3, image_size, image_size))
 
     # Construct the first convolutional pooling layer:
     # filtering reduces the image size to (28-5+1,28-5+1)=(24,24)
     # maxpooling reduces this further to (24/2,24/2) = (12,12)
     # 4D output tensor is thus of shape (batch_size,nkerns[0],12,12)
     layer0 = LeNetConvPoolLayer(rng, input=layer0_input,
-            image_shape=(2500, 3, image_size, image_size),
+            image_shape=(batch_size, 3, image_size, image_size),
             filter_shape=(nkerns[0], 3, 5, 5), poolsize=(2, 2))
 
     # Construct the second convolutional pooling layer
@@ -182,7 +182,7 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=5,
     # maxpooling reduces this further to (8/2,8/2) = (4,4)
     # 4D output tensor is thus of shape (nkerns[0],nkerns[1],4,4)
     layer1 = LeNetConvPoolLayer(rng, input=layer0.output,
-            image_shape=(2500, nkerns[0], 14, 14),
+            image_shape=(batch_size, nkerns[0], 14, 14),
             filter_shape=(nkerns[1], nkerns[0], 5, 5), poolsize=(2, 2))
 
     # the HiddenLayer being fully-connected, it operates on 2D matrices of
@@ -192,10 +192,10 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=5,
 
     # construct a fully-connected sigmoidal layer
     layer2 = HiddenLayer(rng, input=layer2_input, n_in=1250,
-                         n_out=2500, activation=T.tanh)
+                         n_out=50, activation=T.tanh)
 
     # classify the values of the fully-connected sigmoidal layer
-    layer3 = LogisticRegression(input=layer2.output, n_in=2500, n_out=2)
+    layer3 = LogisticRegression(input=layer2.output, n_in=50, n_out=2)
 
     # the cost we minimize during training is the NLL of the model
     cost = layer3.negative_log_likelihood(y)
@@ -208,8 +208,8 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=5,
 
     validate_model = theano.function([index], layer3.errors(y),
             givens={
-                x: valid_set_x,
-                y: valid_set_y}, on_unused_input='ignore')
+                x: valid_set_x[index * batch_size: (index + 1) * batch_size],
+                y: valid_set_y[index * batch_size: (index + 1) * batch_size]})
 
     # create a list of all model parameters to be fit by gradient descent
     params = layer3.params + layer2.params + layer1.params + layer0.params
@@ -228,8 +228,8 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=5,
 
     train_model = theano.function([index], cost, updates=updates,
           givens={
-            x: train_set_x,
-            y: train_set_y}, on_unused_input='ignore')
+            x: train_set_x[index * batch_size: (index + 1) * batch_size],
+            y: train_set_y[index * batch_size: (index + 1) * batch_size]})
 
     ###############
     # TRAIN MODEL #
@@ -259,11 +259,17 @@ def evaluate_lenet5(learning_rate=0.1, n_epochs=5,
     while epoch < n_epochs:
         epoch = epoch + 1
         
-        cost_ij = train_model(1)
+        validation_losses = 0
+        
+        for i in range(0,250):
+			cost_ij = train_model(i)
            
-        validation_losses = validate_model(1)
+			validation_losses += validate_model(i)
+			
+        avg_losses = validation_losses / 250
+		
+        print avg_losses
                 
-        print validation_losses
 
 
     end_time = time.clock()
--- logistic_sgd.py
+++ logistic_sgd.py
@@ -120,6 +120,9 @@ class LogisticRegression(object):
         # the mean (across minibatch examples) of the elements in v,
         # i.e., the mean log-likelihood across the minibatch.
         return -T.mean(T.log(self.p_y_given_x)[T.arange(y.shape[0]), y])
+        
+    def activation(self, y):
+		return T.mean(self.p_y_given_x - y)
 
     def errors(self, y):
         """Return a float representing the number of errors in the minibatch
